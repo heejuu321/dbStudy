f432a53df613e1c32a00209b2803d1bb2d6b6812
heejuu <heejuu321@gmail.com>
2024-01-15 00:51:10 +0900

    커밋4

--- src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -2,13 +2,20 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,24 +24,50 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
  * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화
  */
-class MemberServiceV3_1Test {
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+@SpringBootTest
+class MemberServiceV3_3Test {
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
-    private MemberRepositoryV3 memberRepository;
-    private MemberServiceV3_1 memberService;
-//    @BeforeEach
-//    void before() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-//        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-//        memberRepository = new MemberRepositoryV3(dataSource);
-//          = new MemberServiceV3_1(transactionManager, memberRepository);
-//    }
+    @Autowired
+    MemberRepositoryV3 memberRepository;
+    @Autowired
+    MemberServiceV3_3 memberService;
     @AfterEach
     void after() throws SQLException {
         memberRepository.delete(MEMBER_A);
         memberRepository.delete(MEMBER_B);
         memberRepository.delete(MEMBER_EX);
+    }
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        DataSource dataSource() {
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource());
+        }
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+    @Test
+    void AopCheck() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberRepository class={}", memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
     }
     @Test
     @DisplayName("정상 이체")
@@ -45,7 +78,8 @@ class MemberServiceV3_1Test {
         memberRepository.save(memberA);
         memberRepository.save(memberB);
         //when
-        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        memberService.accountTransfer(memberA.getMemberId(),
+                memberB.getMemberId(), 2000);
         //then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
@@ -62,7 +96,8 @@ class MemberServiceV3_1Test {
         memberRepository.save(memberEx);
         //when
         assertThatThrownBy(() ->
-                memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(),
+                        2000))
                 .isInstanceOf(IllegalStateException.class);
         //then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
